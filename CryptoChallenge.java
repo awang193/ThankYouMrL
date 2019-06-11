db2a489f560d915b60c4c9983b28e67d53e2f042
awang193 <31601228+awang193@users.noreply.github.com>
2019-06-10 21:52:37 -0700

    Created crypto challenge

---
+++ CryptoChallenge.java
@@ -0,0 +1,20 @@
+public class CryptoChallenge
+{
+    private String enc;
+    private String ans;
+
+    public CryptoChallenge(String encrypted, String answer)
+    {
+        enc = encrypted;
+        ans = answer;
+    }
+
+    public void run()
+    {
+        /** Matthew has to implement this */
+
+        // Print prompt: We've received an encrypted message from Bacon Caesar with instructions on how to proceed. It's located below or something like that
+        // Encrypted: BAABBAABBBAAAAAABBABABABA BBAAAABBBABABAA ABBAABAAAB ABABB BABBAAABAA AAAAAABBBBABBBBBAAABAABAAAAABAABAAAAAAAABAABBAABAA BAABBAABBBAABAA BABBAABBBABAAABABABA AAAAAABBABAAABB BAABBABAAAABBAAAABAA BBAAAABBBABABAA ABBBBBABAABAABB ABAAAABBAB AABABABBBABAAAB BABAABAABA AABAABABABAABAABAAABBBAAA AAABBAAAAABBAAA
+        // Decrypted: THANK YOU MR.L! WE APPRECIATE THE WORK AND TIME YOU PUT IN FOR US EVERY DAY!
+    }
+}
