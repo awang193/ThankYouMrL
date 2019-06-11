69ca363a21b9af2ae821e9d5ae8c22ec5a54358a
awang193 <31601228+awang193@users.noreply.github.com>
2019-06-10 21:58:00 -0700

    Deleted unnecessary code in main

--- Game.java
+++ Game.java
@@ -5,18 +5,8 @@ public class Game
 
     }
 
-
-    public void cryptoChallenge()
-    {
-        String encrypted = "BAABBAABBBAAAAAABBABABABA BBAAAABBBABABAA ABBAABAAAB ABABB BABBAAABAA AAAAAABBBBABBBBBAAABAABAAAAABAABAAAAAAAABAABBAABAA BAABBAABBBAABAA BABBAABBBABAAABABABA AAAAAABBABAAABB BAABBABAAAABBAAAABAA BBAAAABBBABABAA ABBBBBABAABAABB ABAAAABBAB AABABABBBABAAAB BABAABAABA AABAABABABAABAABAAABBBAAA AAABBAAAAABBAAA";
-        System.out.println("We've received an encrypted message from Bacon Caesar with instructions on how to proceed. It's located below");
-
-
-    }
-
     public static void main(String[] args)
     {
         Game g = new Game();
-        g.cryptoChallenge();
     }
 }
