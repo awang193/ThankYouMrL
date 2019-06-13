a92cff5c4073b57d42037c5694524d449010ca94
Matthew Oh <39202781+Matthew-Oh@users.noreply.github.com>
2019-06-12 20:17:23 -0700

    Now works with message class

--- MessageView.java
+++ MessageView.java
@@ -1,59 +1,77 @@
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.stage.Modality;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
+import javafx.scene.Group;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.paint.Color;
 import javafx.scene.control.ListView;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.MouseButton;
+import java.util.ArrayList;
 
 /**
  * New Window to view messages
- * @author Matthew Oh,
+ * @author Matthew Oh
  * Thanks for being an awesome teacher!
  */
 public class MessageView
 {
-    ListView<msg> names;
-    Label message;
+    ListView<Message> names;
+    Label text;
     public void display()
     {
         Stage window = new Stage();
         window.initModality(Modality.APPLICATION_MODAL);
-        window.setTitle("potato");
+        window.setTitle("Thank You Messages");
         
         names = new ListView();
-        names.getItems().addAll(
-            new msg("name1", "msg1"), new msg("name2", "msg2"),
-            new msg("name3", "msg3"));
+        ArrayList<Message> messages = MessageLoader.load("test.txt");
+        for (Message m: messages)
+            names.getItems().add(m);
         names.setMinWidth(200);
         names.setMaxWidth(200);
+        names.setMinHeight(500);
+        names.setMaxHeight(500);
         
-        names.setOnMouseClicked(new EventHandler<MouseEvent>()
+        names.setOnMousePressed(new EventHandler<MouseEvent>()
         {
             public void handle(MouseEvent event)
             {
-                msg m = (msg)names.getSelectionModel().getSelectedItems().get(0);
-                //System.out.println(m);
-                message.setText(m.getNote());
+                Message m = (Message)names.getSelectionModel().getSelectedItems().get(0);
+                text.setText(m.getNote());
             }
         });
         
-        message = new Label("Mr L, thanks for allowing me into APCS my sophomore year. I’ve had such a wonderful time in both the class and developing my skills and interacting with my fellow c0d3rs in CS Club. I hope you love your new job as much as you loved us!");
-        message.setWrapText(true);
-        message.setMinWidth(300);
-        message.setMinWidth(300);
+        text = new Label("1\n2\n3\n4\n5\n6\n7\n8\n9\n10");
+        text.setWrapText(true);
+        text.setMinWidth(300);
+        text.setMaxWidth(300);
+        text.setMinHeight(500);
+        text.setMaxHeight(500);
         
+        VBox v = new VBox();
+        v.getChildren().add(text);
+        v.setPadding(new Insets(5));
+        text.setAlignment(Pos.TOP_LEFT);
         HBox h = new HBox();
-        h.getChildren().addAll(names,message);
-        Scene s = new Scene(h, 500,500);
+        h.getChildren().addAll(names,v);
+        //compensate for non-resizable size error
+        Group g = new Group();
+        g.getChildren().addAll(new Rectangle(500,500, Color.WHITE), h);
+        Scene s = new Scene(g);
         
         window.setScene(s);
+        window.sizeToScene();
+        window.setResizable(false);
         window.show();
     }
 }
